d42e63a29737c870356840b939bab5c03a2c5a25
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-09-02 18:50:55 +0530

    refactor: improve tabs config

--- init.lua
+++ init.lua
@@ -1,10 +1,13 @@
 vim.cmd("let g:netrw_liststyle = 3") -- nvim styling.
 
-local opt = vim.opt -- for consiziness.
+local opt = vim.opt -- for conciseness.
 
--- tabs & space
+-- tabs & indentation
+opt.tabstop = 4
 opt.shiftwidth = 4
-opt.smarttab = true
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
 
 -- no mouse
 opt.mouse = ""
