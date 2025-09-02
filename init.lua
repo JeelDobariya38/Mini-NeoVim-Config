91f831fe73496c9bc910267e7dc9ae54aed30d64
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-09-02 13:25:38 +0530

    feat: disable mouse

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@ vim.cmd("let g:netrw_liststyle = 3") -- nvim styling.
 
 local opt = vim.opt -- for consiziness.
 
+-- no mouse
+opt.mouse = ""
+
 -- clipboard settings
 opt.clipboard:append("unnamedplus") -- set system clipboard as defualt.
 
