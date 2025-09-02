3ca89f6f270c050e7bba73370e16422f492e59ea
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-09-02 18:30:48 +0530

    feat: setup tabs & spaces

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@ vim.cmd("let g:netrw_liststyle = 3") -- nvim styling.
 
 local opt = vim.opt -- for consiziness.
 
+-- tabs & space
+opt.shiftwidth = 4
+opt.smarttab = true
+
 -- no mouse
 opt.mouse = ""
 
