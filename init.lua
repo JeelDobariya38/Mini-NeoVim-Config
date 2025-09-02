203942510635e281bad0ba83639cd5e5c9795e16
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-09-02 19:08:09 +0530

    feat: improve appearance

--- init.lua
+++ init.lua
@@ -3,13 +3,22 @@ vim.cmd("let g:netrw_liststyle = 3") -- nvim styling.
 
 local opt = vim.opt -- for conciseness.
 
-
 -- tabs & indentation
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 opt.autoindent = true
 opt.smartindent = true
+
+
+-- cursor line
+opt.cursorline = true
+
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolum = "yes"
 
 
 -- line wrapping
