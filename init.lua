98eba5b84450bce70d6179414e0c54c145fd4a99
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-09-07 15:49:31 +0530

    feat: add shortcuts for window spliting

--- init.lua
+++ init.lua
@@ -20,6 +20,13 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "Open Explorer" })
 
 
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+
 -- no arrow keys
 keymap.set({'n', 'v', 's', 'o'}, '<Up>', ':echo "Nop! No up arrow for you use k!!!"<CR>', {desc = "Hardcore neovim", noremap = true})
 keymap.set({'n', 'v', 's', 'o'}, '<Down>', ':echo "Nop! No down arrow for you use j!!!"<CR>', {desc = "Hardcore neovim", noremap = true})
