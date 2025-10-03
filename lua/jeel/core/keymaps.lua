147572276b8070f2ef7dc0ee867da2e464a42080
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-10-03 10:13:49 +0530

    feat: add lazygit & nvim-tree plugin

--- lua/jeel/core/keymaps.lua
+++ lua/jeel/core/keymaps.lua
@@ -14,10 +14,6 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 
--- for quick accessing explorer
-keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "Open Explorer" })
-
-
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
