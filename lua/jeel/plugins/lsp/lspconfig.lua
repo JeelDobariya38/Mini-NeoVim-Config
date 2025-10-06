e3da888bf064d0fece1570386b74bb2ecf67ea55
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-10-06 17:17:51 +0530

    fix: setup handeler & config error.

--- lua/jeel/plugins/lsp/lspconfig.lua
+++ lua/jeel/plugins/lsp/lspconfig.lua
@@ -6,17 +6,9 @@ return {
         { "antosha417/nvim-lsp-file-operations", config = true },
         { "folke/neodev.nvim", opts = {} },
     },
-    
     config = function()
-        -- import lspconfig plugin
-        local lspconfig = require("lspconfig")
-
-        -- import mason_lspconfig plugin
-        local mason_lspconfig = require("mason-lspconfig")
-
         -- import cmp-nvim-lsp plugin
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
         local keymap = vim.keymap -- for conciseness
 
         vim.api.nvim_create_autocmd("LspAttach", {
@@ -79,33 +71,18 @@ return {
             vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
         end
 
-        mason_lspconfig.setup_handlers({
-            -- default handler for installed servers
-            function(server_name)
-                lspconfig[server_name].setup({
-                    capabilities = capabilities,
-                })
-            end,
-
-            ["lua_ls"] = function()
-                -- configure lua server (with special settings)
-                lspconfig["lua_ls"].setup(
-                    {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                -- make the language server recognize "vim" global
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                                completion = {
-                                    callSnippet = "Replace",
-                                },
-                            },
-                        },
-                    }
-                )
-            end,
+        vim.lsp.config('*', {
+            capabilities = capabilities,
+        })
+        -- Lua configuration
+        vim.lsp.config('lua_ls', {
+            settings = {
+                Lua = {
+                    -- make the language server recognize "vim" global
+                    diagnostics = { globals = { "vim" } },
+                    completion = { callSnippet = "Replace" },
+                },
+            },
         })
     end,
 }
